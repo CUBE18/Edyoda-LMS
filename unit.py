d5115cbc442bcd5516f89066078411f542fe4d7c
CRG <88026296+CUBE18@users.noreply.github.com>
2021-08-08 13:30:12 +0530

    Update unit.py

--- unit.py
+++ unit.py
@@ -6,15 +6,16 @@ from Member import Member
 from Library import Library
 
 
-b1 = Book("an","nn","12",47)
-b2 = Book("21","n123n","122",41237)
+b1 = Book("java","abcd","12",47)
+b2 = Book("python","pqrs","13",48)
+b3 = Book("crg","mnop","14",49)
 print(Book.get_all_books())
-
-
+1
 library = Library(5,5)
-library.add_book_to_rack(b1,1,1234)
-library.add_book_to_rack(b1,1,3455)
-library.remove_book_from_rack(1234)
+library.add_book_to_rack(b1,1,1)
+library.add_book_to_rack(b2,2,1)
+library.add_book_to_rack(b3,1,2)
 
-member1 = Member("shamanth","Banglore",22,889128319,123123,library)
-member2 = Member("Usha","Banglore",22,8899788789,123124,library)
+member1 = Member("CRG","Banglore",22,889128319,123123,library)
+member2 = Member("vinay","mumbai",23,8899788789,123124,library)
+member3 = Member("ibad","delhi",21,889128318,123123,library)
